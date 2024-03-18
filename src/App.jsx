d2b9fd536f20af636c9f158e2d60ace5243c6843
Josue M Darce <133271041+darcejosue@users.noreply.github.com>
2024-03-17 22:21:53 -0600

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,97 @@
-import { Header } from "./components/Header"
-import { Guitar } from "./components/Guitar"
-import { db } from "./data/guitarras"
+import { Header } from "./components/Header";
+import { Guitar } from "./components/Guitar";
+import { db } from "./data/guitarras";
 
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
 function App() {
-
-    const [auth, setAuth] = useState(false);
     
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < 5) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > 1) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+
+    setCart(updatedCart);
+  }
 
   return (
     <>
-    
-   <Header/>
+      <Header
+        cart={cart}
+        setCart={setCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+      />
 
-    <main className="container-xl mt-5">
+      <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {db.map(guitarra => 
-                (<Guitar key={guitarra.id}
-                name={guitarra.name}
-                image={guitarra.image}
-                description={guitarra.description}
-                price={guitarra.price}/>))}
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              cart={cart}
+              setCart={setCart}
+              addToCart={addToCart}
+            />
+          ))}
         </div>
-    </main>
+      </main>
 
-
-    <footer className="bg-dark mt-5 py-5">
+      <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
         </div>
-    </footer>
-
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,10 +1,13 @@
-export const Guitar = ({ name, image, description, price }) => {
+export const Guitar = ({guitar, addToCart}) => {
+
+   const { id, name, image, description, price } = guitar;
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
       <div className="col-4">
         <img
           className="img-fluid"
-          src={`../../public/img/${image}.jpg`}
+          src={`/img/${image}.jpg`}
           alt="imagen guitarra"
         />
       </div>
@@ -14,7 +17,8 @@ export const Guitar = ({ name, image, description, price }) => {
           {description}
         </p>
         <p className="fw-black text-primary fs-3">${price}</p>
-        <button type="button" className="btn btn-dark w-100">
+        <button type="button" className="btn btn-dark w-100"
+        onClick={()=>addToCart(guitar)}>
           Agregar al Carrito
         </button>
       </div>
